48eb2bde8a35c7cbe0a98d2f16d4657ac87c2466
Hakim Hanif <hakim.hanif@gmail.com>
2014-06-05 22:54:54 -0700

    basic user model with authentication support

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140530063414) do
+ActiveRecord::Schema.define(version: 20140606054318) do
 
   create_table "user_card_entries", force: true do |t|
     t.string   "entry_key"
@@ -28,6 +28,14 @@ ActiveRecord::Schema.define(version: 20140530063414) do
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
