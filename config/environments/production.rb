c23d9b5f4485960d56976f0c24a8b68428dcc5a5
Hakim Hanif <hakim.hanif@gmail.com>
2014-08-03 22:34:05 -0700

    fixing the url for forgot password and subject of email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Learnbird::Application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { :host => "excitest.com", :protocol => "https"  }
+  config.action_mailer.default_url_options = { :host => "www.excitest.com", :protocol => "https"  }
 
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
